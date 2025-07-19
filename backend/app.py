1c57afb89712abd530862a1f63acb78e98d85846
gustigratia <gustigratia706@gmail.com>
2025-07-19 14:01:34 +0700

    feat: add backend server using flask

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import joblib
+
+app = Flask(__name__)
+CORS(app)
+
+model = joblib.load("../model/logistic_model.pkl")
+vectorizer = joblib.load("../model/tfidf_vectorizer.pkl")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    text = data.get('text', '')
+    
+    vectorized = vectorizer.transform([text])
+    prediction = model.predict(vectorized)[0]
+    
+    return jsonify({"sentiment": prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
