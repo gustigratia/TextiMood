db53adddc0d627c5fe6504d7cd1c121f3115ed46
gustigratia <gustigratia706@gmail.com>
2025-07-20 21:45:51 +0700

    fix: update env config

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -19,4 +20,5 @@ def predict():
     return jsonify({"result": prediction})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
